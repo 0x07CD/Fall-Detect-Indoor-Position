93d5006b94effd8449f4fe886bfab69e96e28500
0x07CD <grishacker@gmail.com>
2019-11-14 07:07:31 +0700

    add Complementary filter

--- WearableDevice/WearableDevice.ino
+++ WearableDevice/WearableDevice.ino
@@ -5,6 +5,7 @@
 #include <utility/MPU9250.h>
 
 #define EDDY_UUID   0xFEAA
+#define DELTA_TIME  0.02        // 20 ms sample rate
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,6 +17,11 @@ uint8_t temprature_sens_read();
 }
 #endif
 
+// Prototype-Function
+void setupBeacon();
+void fallDetectTask(void *pvParameters);
+void advertiseBeaconTask(void *pvParameters);
+
 // Global Variable
 BLEAdvertising *pAdvertising;
 MPU9250 IMU;
@@ -23,6 +29,29 @@ TaskHandle_t fall_detect_task_handle;
 TaskHandle_t advertise_beacon_task_handle;
 struct timeval now;
 uint32_t bootcount;
+
+// M5 for test
+void setup() {
+  M5.begin();
+  Wire.begin();
+  IMU.initMPU9250();
+  IMU.initAK8963(IMU.magCalibration);
+  IMU.calibrateMPU9250(IMU.gyroBias, IMU.accelBias);
+  Serial.begin(115200);
+  M5.Lcd.print("Device Start...\n");
+  // Create the BLE Device
+  BLEDevice::init("Wearable");
+  // Create the BLE Server
+  pAdvertising = BLEDevice::getAdvertising();
+
+  // Create Task
+  xTaskCreate(fallDetectTask, "Fall-Detection Task", 10000, NULL, 2, &fall_detect_task_handle);
+  xTaskCreate(advertiseBeaconTask, "Advertising Beacon Task", 10000, NULL, 1, &advertise_beacon_task_handle);
+}
+
+void loop() {
+   
+}
 
 // setup eddystone beacon
 void setupBeacon() {
@@ -62,34 +91,75 @@ void setupBeacon() {
   pAdvertising->setScanResponseData(advertisementData);
 }
 
+void complementaryFilter(){
+  
+}
+
 // Task for Fall-Detection
 void fallDetectTask(void *pvParameters){
-  float total_accel = 0.0f;
+  float accelRoll, accelPitch;
+  float roll, pitch;
+  float passRoll, passPitch;
   while(1){
     if (IMU.readByte(MPU9250_ADDRESS, INT_STATUS) & 0x01)
     {
       M5.Lcd.setCursor(0, 10);
       IMU.readAccelData(IMU.accelCount);
+      IMU.readGyroData(IMU.gyroCount);
+      // IMU.readMagData(IMU.magCount);
       IMU.getAres(); // get accelerometer scales saved to "aRes"
+      IMU.getGres(); // get gyroscope scales saved to "gRes"
+      // IMU.getMres(); // get megnetometor scales saved to "mRes"
+      
       IMU.ax = (float)IMU.accelCount[0] * IMU.aRes; // - accelBias[0];
       IMU.ay = (float)IMU.accelCount[1] * IMU.aRes; // - accelBias[1];
       IMU.az = (float)IMU.accelCount[2] * IMU.aRes; // - accelBias[2];
-      M5.Lcd.print("X-acceleration: ");
-      M5.Lcd.print(1000 * IMU.ax);
-      M5.Lcd.println(" mg ");
-      M5.Lcd.print("Y-acceleration: ");
-      M5.Lcd.print(1000 * IMU.ay);
-      M5.Lcd.println(" mg ");
-      M5.Lcd.print("Z-acceleration: ");
-      M5.Lcd.print(1000 * IMU.az);
-      M5.Lcd.println(" mg ");
 
-      total_accel = sqrt(IMU.ax*IMU.ax + IMU.ay*IMU.ay + IMU.az*IMU.az);
-      M5.Lcd.print("Total acceleration: ");
-      M5.Lcd.print(total_accel);
-      M5.Lcd.println(" mg ");
+      IMU.gx = (float)IMU.gyroCount[0] * IMU.gRes;
+      IMU.gy = (float)IMU.gyroCount[1] * IMU.gRes;
+      IMU.gz = (float)IMU.gyroCount[2] * IMU.gRes;
+      
+      // IMU.mx = (float)IMU.magCount[0] * IMU.mRes * IMU.magCalibration[0] - IMU.magbias[0];
+      // IMU.my = (float)IMU.magCount[1] * IMU.mRes * IMU.magCalibration[1] - IMU.magbias[1];
+      // IMU.mz = (float)IMU.magCount[2] * IMU.mRes * IMU.magCalibration[2] - IMU.magbias[2];
+
+      accelRoll = (float)(atan2(IMU.ay, IMU.az)) * RAD_TO_DEG;
+      accelPitch = (float)(atan2(-IMU.ax, sqrt(pow(IMU.ay, 2) + pow(IMU.az, 2)))) * RAD_TO_DEG;
+      // magX = IMU.mx * cos(accelPitch) + IMU.mz * sin(accelPitch);
+      // magY = IMU.mx * sin(accelRoll) * sin(accelPitch) + IMU.my * cos(accelRoll) - IMU.mz * sin(accelRoll) * cos(accelPitch); 
+      // magYaw = (float)(atan2(magY, magX)) * RAD_TO_DEG;
+
+      passRoll = roll;
+      passPitch = pitch;
+      
+      // Complemetary Filter
+      roll = 0.98 * (roll + IMU.gx * DELTA_TIME) + 0.02 * accelRoll;
+      pitch = 0.98 * (pitch + IMU.gy * DELTA_TIME) + 0.02 * accelPitch;
+      // yaw = 0.98 * (yaw + IMU.gz * DELTA_TIME) + 0.02 * magYaw;
+
+      // for plot graph
+      Serial.print("Roll\t");
+      Serial.print(roll);
+      Serial.print("\tPitch\t");
+      Serial.println(pitch);
+      // Serial.print("\tYaw\t");
+      // Serial.println(yaw);
+
+      M5.Lcd.print("Roll: ");
+      M5.Lcd.println(roll);
+      M5.Lcd.print("Pitch: ");
+      M5.Lcd.println(pitch);
+      // M5.Lcd.print("Yaw: ");
+      // M5.Lcd.println(yaw);
+
+      /* Fall Detect
+       *  
+       *  code
+       *  
+       */
+
     }
-    vTaskDelay(50 / portTICK_PERIOD_MS); // 0.05 milisecond
+    vTaskDelay(20 / portTICK_PERIOD_MS); // 0.02 second
   }
 }
 
@@ -100,32 +170,10 @@ void advertiseBeaconTask(void *pvParameters){
     gettimeofday(&now, NULL);
     setupBeacon();
     pAdvertising->start();
-    Serial.println("Start Advertising Beacon...");
-    vTaskDelay(4000 / portTICK_PERIOD_MS); // for Advertise 4 milisecond
+    // Serial.println("Start Advertising Beacon...");
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // for Advertise 1 second
     pAdvertising->stop();
-    Serial.println("Stop Advertising Beacon...");
-    vTaskDelay(1000 / portTICK_PERIOD_MS); // for break 1 milisecond
+    // Serial.println("Stop Advertising Beacon...");
+    vTaskDelay(1000 / portTICK_PERIOD_MS); // for break 1 second
   }
-}
-
-// M5 for test
-void setup() {
-  M5.begin();
-  Wire.begin();
-  IMU.initMPU9250();
-  IMU.calibrateMPU9250(IMU.gyroBias, IMU.accelBias);
-  Serial.begin(115200);
-  M5.Lcd.print("Device Start...\n");
-  // Create the BLE Device
-  BLEDevice::init("Wearable");
-  // Create the BLE Server
-  pAdvertising = BLEDevice::getAdvertising();
-
-  // Create Task
-  xTaskCreate(fallDetectTask, "Fall-Detection Task", 10000, NULL, 2, &fall_detect_task_handle);
-  xTaskCreate(advertiseBeaconTask, "Advertising Beacon Task", 10000, NULL, 1, &advertise_beacon_task_handle);
-}
-
-void loop() {
-   
 }
